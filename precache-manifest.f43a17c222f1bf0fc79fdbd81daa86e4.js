d70474bf1dce992ba918a253c428359ead988181
unknown <cyberviolate@gmail.com>
2019-12-17 16:19:04 +0330

    Updates

---
+++ precache-manifest.f43a17c222f1bf0fc79fdbd81daa86e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1445fecd06bf8aeebe5cabb3b8197add",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "69e25f9a7af3c76f743f",
+    "url": "/monster/static/css/main.880eaa1e.chunk.css"
+  },
+  {
+    "revision": "d2396aeab0ab74cef20c",
+    "url": "/monster/static/js/2.f2b95054.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster/static/js/2.f2b95054.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69e25f9a7af3c76f743f",
+    "url": "/monster/static/js/main.e270de8b.chunk.js"
+  },
+  {
+    "revision": "26fff4bf3382c85dccf1",
+    "url": "/monster/static/js/runtime-main.511d314b.js"
+  }
+]);
